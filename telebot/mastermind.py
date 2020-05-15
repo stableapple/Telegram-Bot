8af2af49530f699d06c98f21201d043cb57d70ce
stableapple <riddhimenon@am.students.amrita.edu>
2020-05-16 01:04:32 +0530

    added screenshot

--- telebot/mastermind.py
+++ telebot/mastermind.py
@@ -1,16 +1,16 @@
 import urllib.request, json
 
 
-def get_response(user_input):
+def get_response():
     url = "https://api.covid19api.com/summary"
     response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
+    data = json.dumps(response, sort_keys=True,indent=4, separators=(',',':'))
     for i in range(len(data['Countries'])):
         key,value = list(data['Countries'][i].items())[0]
-        if value==user_input:
+        if value=="India":
             x=data['Countries'][i]
             tmpstring = ""
             for key, value in x.items():
                 tmpstring = tmpstring + "{0:<20} {1}".format(key, value) + "\n"
             return tmpstring
-
+get_response()
